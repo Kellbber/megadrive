2590d1201d098e752c58a83dc5b8fe16aae82b30
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-18 20:00:50 -0300

    feat - implement findOne and chore App - swagger

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,25 +1,29 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CreateTableDto } from "./dto/create-table.dto";
-import { TableService } from "./table.service";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Table } from 'src/entities/table.entity';
+import { CreateTableDto } from './dto/create-table.dto';
+import { TableService } from './table.service';
 @ApiTags('table')
 @Controller('table')
 export class TableController {
   constructor(private readonly tableService: TableService) {}
 
   @Get()
-  findAll(){
+  @ApiOperation({
+    summary: 'Listar todas as Salas'
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
-
+  }
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Visualizar uma sala única por ID'
+  })
+  findOne(@Param('id') id:string): Promise<Table>{
+    return this.tableService.findOne(id)
   }
   @Post()
-  create(@Body() dto: CreateTableDto){
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
-
   }
-
 }
-
-
-
-
