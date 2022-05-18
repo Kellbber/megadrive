08ce7928fc89e43a0d75256134b3d01211954007
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-18 17:25:27 -0300

    refactor - create module & controller

---
+++ src/table/table.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller('table')
+export class TableController {
+  @Get()
+  findAll(){
+    return 'Buscar todas as salas;'
+  }
+}
+
+
---
+++ src/table/table.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { TableController } from "./table.controller";
+
+
+@Module({
+ controllers: [TableController],
+ providers: [],
+})
+export class TableModule {}
