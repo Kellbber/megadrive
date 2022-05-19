ffc33f5ee8c46d88c1d543219831a03495f0edaa
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-19 16:28:25 -0300

    chore - table - implement NF error Update

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -6,27 +6,28 @@ import { UpdateTableDto } from './dto/update-table.dto';
 
 @Injectable()
 export class TableService {
-
-
-
-
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<Table[]> {
     return this.prisma.table.findMany();
   }
 
-  async findOne(id: string): Promise<Table>{
+  async findById(id: string): Promise<Table>{
     const record = await this.prisma.table.findUnique({
       where: {
-        id: id
-      }
+        id: id,
+      },
     });
-    if(!record){
-      throw new NotFoundException(`Registro com o ID: '${id}' não encontrado.`)
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID: '${id}' não encontrado.`);
     }
 
-    return record
+    return record;
+  }
+
+  async findOne(id: string): Promise<Table> {
+
+    return this.findById(id);
   }
   create(dto: CreateTableDto): Promise<Table> {
     const data: Table = { ...dto };
@@ -36,20 +37,20 @@ export class TableService {
     });
   }
 
+  async update(id: string, dto: UpdateTableDto): Promise<Table> {
+    await this.findById(id)
+    const data: Partial<Table> = { ...dto };
 
-  update(id: string, dto: UpdateTableDto): Promise<Table> {
-    const data: Partial<Table> ={...dto};
-
-   return this.prisma.table.update({
-     where: {id: id},
-    data,
-   })
+    return this.prisma.table.update({
+      where: { id: id },
+      data,
+    });
   }
   async delete(id: string) {
-  await this.prisma.table.delete({
-     where: {
-       id: id
-     }
-   })
+    await this.prisma.table.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
