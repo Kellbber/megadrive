1ad614159c52f8d32fbb070c4dcbf41407f5a50f
Kellbber <kellbberbarcarolo@gmail.com>
2022-05-23 16:37:24 -0300

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,8 +1,33 @@
-export class CreateProductDto {
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString, IsUrl } from "class-validator";
 
+export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'nome do produto',
+    example: 'Pizza de Strogonoff',
+  })
   name: string;
+  @IsString()
+  @ApiProperty({
+    description: 'descrição do produto',
+    example: 'Queijo, carne de gado, tomate e especiarias...',
+  })
   description: string;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'preço do produto',
+    example: 67.90,
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
   image: string;
 
 }
